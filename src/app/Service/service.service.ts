b3d45a57511b4df3877e6359618be456e7aad717
koynewton <nicole.newton@alumnos.udg.mx>
2019-08-20 12:35:02 -0500

    hacer modelo de persona y agregar servicio

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Persona } from '../Modelo/Persona';
+
+
+// se encarga de hacer la conexión con el back-end
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  // Hace referencia al backend
+  Url = "http://localhost:8080/CrudSpring/personas";
+  
+  getPersonas(){
+    return this.http.get<Persona[]>(this.Url);
+  }
 }
